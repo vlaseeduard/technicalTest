4da1e6cbe6dd03b937d0d50d0bc0427e682e616f
eduard vlase <eduard.vlase@dounami.co.uk>
2021-07-27 20:45:03 +0100

    -added the SECRET_KEY to the environment variables

--- technicalTest/technicalTest/settings.py
+++ technicalTest/technicalTest/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vlamt)-*p()+ot^3=6fe_n18fb#05h_sbu11n#!3=jtp*o&57!'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
